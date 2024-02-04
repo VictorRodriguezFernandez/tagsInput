665a192f4219680c7519f689b1190c4bba37036f
VictorRodriguezFernandez <Serkone@hotmail.com>
2024-02-04 13:43:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import InputTag from "./components/InputTag.vue";
+
+export default{
+  components: {
+    InputTag,
+  },
+  methods: {
+    handleOnTagsChange(tags) {
+      console.log(tags);
+    },
+  },
+};
+
+</script>
+
+<template><InputTag @onTagsChange="handleOnTagsChange" />etiqueta</template>
+ 
+<style scoped></style>
